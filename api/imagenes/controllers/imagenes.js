dad00405ba6c779bde0f82d7044989eea265e41d
diana yela <Dianayela672@gmail.com>
2020-10-28 17:27:59 -0500

    parcial 2

---
+++ api/imagenes/controllers/imagenes.js
@@ -0,0 +1,37 @@
+'use strict';
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+    me: async(ctx)=> {
+        const user = ctx.state.user
+
+        let imagenes = await strapi.services.imagenes.find({
+            user: user.id
+        })
+        ctx.send(imagenes)
+    },
+    create: async(ctx) => {
+        const user = ctx.state.user
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx)
+            data.user = user.id
+
+            entity  = await strapi.services.imagenes.create(data, {
+                files,
+            })
+        }else{
+            const data = ctx.request.body 
+            data.user = user.id
+            entity = await strapi.services.imagenes.create(ctx.request.body);
+
+        }
+
+        return sanitizeEntity(entity, { model: strapi.models.imagenes });
+
+    }
+};
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '843c7b2e-20f3-483b-9203-5a1e06604fe1'
+};
